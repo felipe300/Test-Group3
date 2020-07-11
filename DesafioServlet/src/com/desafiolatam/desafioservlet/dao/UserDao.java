37a0244c7a1aa10fa808d912ce0180bc5170217c
Pipo <caracoloro@gmail.com>
2020-07-11 14:26:40 -0400

    Fixed UserDao

--- DesafioServlet/src/com/desafiolatam/desafioservlet/dao/UserDao.java
+++ DesafioServlet/src/com/desafiolatam/desafioservlet/dao/UserDao.java
@@ -10,24 +10,25 @@ import com.desafiolatam.desafioservlet.entities.UserDTO;
 public class UserDao {
 	
 	public int insertUser(UserDTO dto) throws Exception{
+		int resultado = 0;
 		
-		String sql = "INSERT INTO pg.Usuario ( name, email, pass, isAdmin) VALUES (?, ?, ?, ?)";
+		String sql = "INSERT INTO pg.Usuario (name, email, pass, isAdmin) VALUES (?, ?, ?, ?)";
 		
 		Connection conexion = null;
 		conexion = DaoUtil.getConnection("jdbc/DesafioServlet");
 		
 		try(PreparedStatement stmt = conexion.prepareStatement(sql)){
 			
-			ResultSet resultado = stmt.executeQuery();
-			while(resultado.next()) {
-				stmt.setString(1,dto.getName());
-				stmt.setString(2, dto.getEmail());
-				stmt.setString(3, dto.getPass());
-				stmt.setBoolean(4,dto.isAdmin());
+			stmt.setString(1, dto.getName());
+			stmt.setString(2, dto.getEmail());
+			stmt.setString(3, dto.getPass());
+			stmt.setBoolean(4,dto.isAdmin());
 				
-				if(stmt.executeUpdate() !=1) {
-					throw new RuntimeException("A ocuurido un error inesperado" );
-				}
+			//resultado = stmt.executeQuery();
+			resultado = stmt.executeUpdate();
+				
+			if(stmt.executeUpdate() !=1) {
+				throw new RuntimeException("A ocuurido un error inesperado" );
 			}	
 			
 		}catch(Exception ex) {
@@ -35,10 +36,7 @@ public class UserDao {
 			throw new RuntimeException("A ocuurido un error inesperado " +ex);
 		}
 		
-		return 0;
-		
+		return resultado;		
 	}
-	
-	
 
 }
